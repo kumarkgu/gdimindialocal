6997f6046ac4ca532a6405228f97bcbfcda03633
Rachael.wang <Rachael.wang@ap.jll.com>
2018-08-17 19:39:44 +0900

    XymaxKansai

--- base/pdf/jp_xymaxKansai.py
+++ base/pdf/jp_xymaxKansai.py
@@ -14,37 +14,96 @@ class XymaxKansai(JapanBasePDF):
         )
         self.begin = '種類'
         self.end = None
-        self.pivotcolumn = 3
-        self.regexppivot = re.compile('満室|', re.ASCII)
+        self.pivotcolumn = 5
+        self.regexppivot = re.compile("[A-Za-z]*[0-9]+[,]*[0-9]*F|^\d\d\d$|^満室$|^満車$|^必要な坪数をリクエストください。$|^単独棟$", re.ASCII)
         self.runtype = 'lattice'
 
     @staticmethod
-    def _set_record(currline, prevline,prevprevline ):
+    def _set_header(currline):
         templine = currline
-        if len([field for field in templine if field != ""]) != 1:
-            return templine
-        else:
-            # split column by ";"
-            for index in [12, 3, 1]:
-                if ";" in prevline[index]:
-                    prevline.insert(index + 1, prevline[index].split(";")[1])
-                    prevline[index] = prevline[index].split(";")[0]
-                if prevline[index] == "":
-                    prevline.insert(index, "")
-            # insert 共益費 into column
+        try:
             for index in range(0, len(templine)):
-                if templine[index] != "":
-                    prevline.insert(11, templine[index])
-            #only keep one エリア value
-            if ";" in prevline[0]:
-                prevline[0] = prevline[0].split(";")[0]
+                if index == 1:
+                    templine[index] = templine[index] + ""
+                templine[index] = templine[index].replace(";","")
+        except IndexError:
+            pass
+        templine.insert(1, "Location")
+        templine.insert(4, "Address")
+        return templine
 
-            # auto file first 6 columns for lines without 物 件 名 称
-            if prevprevline and prevline[2] == "":
-                for index in range(0 , 6):
-                    if prevprevline[index] != "" and prevline[index] == "":
-                        prevline[index] = prevprevline[index]
-            return prevline
+    @staticmethod
+    def _set_record(currline, prevline):
+        templine = currline
+        insertstart = 0
+        if prevline is None:
+            return templine
+        try:
+            # In some page, only one area under  エリア
+            if any(str in templine[1] for str in ("PM", "リーシング")):
+                templine[0] = templine[1]
+                templine[1] = templine[2]
+                templine[2] = "-"
+            # In some page, only one area under  エリア, and no 種類
+            if templine[0] == "" and templine[1] == "" and templine[2] != "":
+                templine[0] = "-"
+                templine[1] = templine[2]
+                templine[2] = "-"
+            #if 満室 or 必要な坪数をリクエストください。, need to insert 7 blank value to make columns align
+            if "必要な坪数をリクエストください。" in templine[4]:
+                templine = templine[:5] + ["", "", "", "", "", "", ""] + templine[5:]
+            #split Address and ビル名
+            if ";" in templine[3]:
+                templine.insert(4, ";".join(templine[3].split(";")[1:]))
+                templine[3] = templine[3].split(";")[0]
+            else: templine.insert(4, "")
+            #Only keep one location name
+            if ";" in templine[1]:
+                templine[1] = templine[1].split(";")[0]
+
+            for index in range (0, len(templine)):
+                if templine[index] == "即入居可能" and index != 8:
+                    for i in range(0, 8 - index):
+                        templine.insert(index, "")
+                print(index)
+                print(templine[index])
+
+                if re.search(("^空.*台$|^なし$"), templine[index]) and index != 12:
+                    for i in range(0, 12 - index):
+                        templine.insert(index, "")
+
+                if any(templine[index] == str for str in ("個別", "セントラル", "併用","設置なし")):
+                     if index < 14:
+                         #if previous line is contact number
+                         if ";" in templine[index - 1]:
+                             if templine[index - 2] != "満室":
+                                 insertstart = index - 2
+                             else: insertstart = index - 1
+                         else: insertstart = index
+                         for i in range(0, 14 - index):
+                             templine.insert(insertstart, "")
+                     if index > 14:
+                         for i in range(14, index):
+                             del templine[i -1]
+                             templine.insert(len(templine), "")
+
+                #Autofill data from previous line
+                if index in [0,1,2,3,4,12,13]:
+                    if templine[index] == "" and prevline[index] != "":
+                        templine[index] = prevline[index]
+
+
+            if templine[7] == '' and templine[5] not in ("満室","満車","必要な坪数をリクエストください。") :
+                templine[7] = prevline[7]
+
+            return templine
+        except IndexError:
+            pass
+
+        #個別, セントラル, 併用
+        #応相談
+        #即入居可能
+
 
     def _process_csv(self, infile, outfile):
         w_fileno = open(outfile, 'wt', encoding='utf-8', newline="")
@@ -59,9 +118,15 @@ class XymaxKansai(JapanBasePDF):
                     print('no = '+ str(lineno))
                     print(outline)
                     print(prevline)
-                    writer.writerow(outline)
+                    if lineno < 2 and outline[0] == "種類":
+                        outline = self._set_header(outline)
+                        writer.writerow(outline)
+                        prevline = outline
+                    elif lineno > 0 and len(outline) > 10 and outline[0] not in ("種類","エリア") and outline[4] != "":
+                        outline = self._set_record(outline,prevline)
+                        writer.writerow(outline)
+                        prevline = outline
                     lineno += 1
-                    prevline = outline
         except:
             raise
         finally:
@@ -96,7 +161,7 @@ class XymaxKansai(JapanBasePDF):
                 cp.arrange_csv(
                     tempfile, outfile=tempwriter, pivotcol=self.pivotcolumn,
                     pivotregexp=self.regexppivot, ignorecounter=1,
-                    multipage=True, fileno=filecounter, headercol = 2,
+                    multipage=True, fileno=filecounter, headercol = 0,
                 )
                 filecounter += 1
         except Exception:
